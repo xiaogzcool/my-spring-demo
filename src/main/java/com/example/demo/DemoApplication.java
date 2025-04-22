a3518f2b1b60f3eab1d5327c6bb9363fc9ab7418
zyx <2579249011@qq.com>
2025-04-22 22:46:24 +0800

    fix

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.my-spring-demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.my-spring-demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
